884aff236efee1e7ea839fa7f559d0d0bd983707
Adrian Kumbrink <adrian.kumbrink@gmx.de>
2022-02-21 09:26:05 +0100

    [WIP] Work on compute shader for the background

---
+++ Tower-of-Convos/Assets/Scripts/Background/BoundaryBackground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryBackground : MonoBehaviour
+{
+    ComputeShader shader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void RegenerateTexture()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
