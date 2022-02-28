751bbe1b2c04193c2f2ea1afd859d1c0da28cb48
Kauruck <adrian.kumbrink@gmx.de>
2022-02-28 11:04:21 +0100

    [WIP] Work on Bezier

---
+++ Tower-of-Convos/Assets/Scripts/Path/BezierHelper.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierHelper
+{
+    public const int lengthRes = 100;
+    public static Vector2 calcPosition(BezierCurve curve, float t){
+        t = Mathf.Clamp01(t);
+
+        float x = ((1 - t) * (1 - t) * (1 - t)) * curve.pointA.x
+           + 3 * ((1 - t) * (1 - t)) * t * curve.pointB.x
+           + 3 * (1 - t) * (t * t) * curve.controllA.x
+           + (t * t * t) * curve.controllB.x;
+
+        float y = ((1 - t) * (1 - t) * (1 - t)) * curve.pointA.y
+           + 3 * ((1 - t) * (1 - t)) * t * curve.pointB.y
+           + 3 * (1 - t) * (t * t) * curve.controllA.y
+           + (t * t * t) * curve.controllB.y;
+
+        return new Vector2(x,y);
+    }
+
+    public static float lengthOfCurve(BezierCurve curve){
+        float[] lengthArray = new float[lengthRes + 1];
+        lengthArray[0] = 0;
+        Vector2 prevPos=calcPosition(curve, 0);
+        float  cLength = 0;
+        float stepSize = 1f/lengthRes;
+        for(int i = 1; i <= lengthRes; i++){
+            Vector2 point = calcPosition(curve, stepSize * i);
+            float dx = prevPos.x - point.x;
+            float dy = prevPos.y - point.y;
+            cLength += Mathf.Sqrt(dx * dx + dy * dy);
+            lengthArray[i] = cLength;
+            prevPos = point;
+        }
+
+        return cLength;
+    }
+
+    public float lengthOfCurve(BezierCurve curve, out float[] parts){
+        float[] lengthArray = new float[lengthRes];
+        lengthArray[0] = 0;
+        Vector2 prevPos=calcPosition(curve, 0);
+        float  cLength = 0;
+        float stepSize = 1/lengthRes;
+        for(int i = 1; i <= lengthRes; i++){
+            Vector2 point = calcPosition(curve, stepSize * i);
+            float dx = prevPos.x - point.x;
+            float dy = prevPos.y - point.y;
+            cLength += Mathf.Sqrt(dx * dx + dy * dy);
+            lengthArray[i] = cLength;
+            prevPos = point;
+        }
+
+        parts = lengthArray;
+        return cLength;
+    }
+}
