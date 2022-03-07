1f45aa1fe6dbd5ee0098b6c1111901473a707817
Kauruck <adrian.kumbrink@gmx.de>
2022-03-07 15:11:57 +0100

    [WIP] Work on bezir curve editor - There is a StackOverflow some where

---
+++ Tower-of-Convos/Assets/Scripts/Editor/PathEditor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathHolder))]
+public class PathEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+    }
+
+
+
+    public void OnSceneGUI(){
+        PathHolder holder = (PathHolder) target;
+        Handles.color = Color.red;
+        #region BezierHandelsLine
+        foreach(BezierCurve current in holder.curves){
+            Handles.DrawLine(current.pointA, current.controllA);
+            Handles.DrawLine(current.pointB, current.controllB);
+        }
+        #endregion
+        #region BezierCurves
+        int res = 20;
+        float stepSize = 1f/res;
+        Vector2 oldPos = BezierHelper.calcPosition(holder.curves[0], 0);
+        foreach(BezierCurve current in holder.curves) {
+            for(int i = 0; i <= res; i++){
+                Vector2 newPos = BezierHelper.calcPosition(current, stepSize * i);
+                Handles.DrawLine(oldPos, newPos);
+                oldPos = newPos;
+            }
+        }
+        #endregion
+        #region Handels
+        for(int i = 0; i < holder.curves.Length * 4; i++){
+            EditorGUI.BeginChangeCheck();
+            Vector3 currentPos = Handles.PositionHandle(holder[i], Quaternion.identity);
+            if(EditorGUI.EndChangeCheck()){
+                Undo.RecordObject(holder, "Moved curve controll points");
+                holder[i] = currentPos;
+            }
+        }
+        #endregion
+    }
+    
+}
--- Tower-of-Convos/Assets/Scripts/Path/PathHolder.cs
+++ Tower-of-Convos/Assets/Scripts/Path/PathHolder.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PathHolder : MonoBehaviour
 {
 
     public BezierCurve[] curves = new BezierCurve[1];
     public Texture2D texture;
+
+    public Vector3 test;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,30 +17,68 @@ public class PathHolder : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         
     }
 
+    public Vector2 this[int index]{
+        set => setPostionFromIndex(index, value);
+        get => getPostionFromIndex(index);
+    }
 
-    void OnDrawGizmosSelected(){
-        if(curves.Length > 0){
-            Gizmos.color = Color.red;
-            int res = 20;
-            float stepSize = 1f/res;
-            Vector2 oldPos = BezierHelper.calcPosition(curves[0], 0);
-            foreach(BezierCurve current in curves) {
-                for(int i = 0; i <= res; i++){
-                    Vector2 newPos = BezierHelper.calcPosition(current, stepSize * i);
-                    Gizmos.DrawLine(oldPos, newPos);
-                    oldPos = newPos;
-                }
+    public int Length {
+        get => this.curves.Length * 4;
+    }
 
-                Gizmos.DrawWireSphere(current.pointA, 0.5f);
-                Gizmos.DrawWireSphere(current.pointB, 0.5f);
-                Gizmos.DrawWireSphere(current.controllA, 0.5f);
-                Gizmos.DrawWireSphere(current.controllB, 0.5f);
+    private void setPostionFromIndex(int index, Vector3 value){
+        try{
+            int curveIndex = Mathf.FloorToInt(index/4f);
+            BezierCurve curve = this.curves[curveIndex];
+            switch (index % 4){
+                case 0:
+                    curve.pointA = value;
+                    break;
+                case 3:
+                    curve.pointB = value;
+                    break;
+                case 1:
+                    curve.controllA = value;
+                    break;
+                case 2:
+                    curve.controllB = value;
+                    break;
+            }
+            this.curves[curveIndex] = curve;
+            if(index % 4 == 0 && index > 1){
+                setPostionFromIndex(index - 1, value);
+            }
+            else if(index % 4 == 3 && index < this.Length - 1){
+                setPostionFromIndex(index + 1, value);
             }
         }
+        catch(Exception e){
+            Debug.Log(e.ToString());
+        }
     }
+
+    private Vector2 getPostionFromIndex(int index){
+        int curveIndex = Mathf.FloorToInt(index/4f);
+        BezierCurve curve = this.curves[curveIndex];
+        switch (index % 4){
+            case 0:
+                return curve.pointA;
+            case 3:
+                return curve.pointB;
+            case 1:
+                return curve.controllA;
+            case 2:
+                return curve.controllB;
+            
+            //Never happens
+            default:
+                return curve.pointA;
+        }
+    }
+
 }
