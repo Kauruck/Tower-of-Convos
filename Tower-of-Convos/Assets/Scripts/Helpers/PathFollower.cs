0067aa04b352df9ae6a2f834094ce16263ea90f3
Adrian Kumbrink <adrian.kumbrink@gmx.de>
2022-03-27 14:57:43 +0200

    Better Pathfollower - Also fix glitch in backgorundtexture

--- Tower-of-Convos/Assets/Scripts/Helpers/PathFollower.cs
+++ Tower-of-Convos/Assets/Scripts/Helpers/PathFollower.cs
@@ -10,27 +10,43 @@ public class PathFollower : MonoBehaviour
 
     public float x = 0;
 
+    public float maxAngle = 90;
+
+    public SpriteRenderer spriteToFlip;
+
     private float LookAtOffset = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(path != null){
+        if (path != null)
+        {
             Vector3 pos = path.getPointOnCurves(x);
             pos.z = 2;
             this.transform.position = pos;
-            if(x + LookAtOffset <= path.PathLength){
+            if (x + LookAtOffset <= path.PathLength)
+            {
                 transform.right = ((Vector3)path.getPointOnCurves(x + LookAtOffset)) - new Vector3(transform.position.x, transform.position.y, 0);
+                float angle = Vector3.Angle(transform.right, Vector3.right);
+                if (angle > maxAngle)
+                {
+                    transform.right = -transform.right;
+                    spriteToFlip.flipX = true;
+                }
+                else
+                {
+                    spriteToFlip.flipX = false;
+                }
             }
             x += Speed;
-            if(x >= path.PathLength)
+            if (x >= path.PathLength)
                 x = path.PathLength;
-            }
+        }
     }
 }
